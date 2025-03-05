d3041d42336247780ae1874d74964256b3da72c9
Anna Taranchuk <taranchuk4@yahoo.com>
2025-03-05 11:12:39 +0100

    chore: init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ChakraProvider value={defaultSystem}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </StrictMode>
+);
