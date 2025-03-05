8901d68785ca121689b2ec46ea0cc289bb696ed6
Anna Taranchuk <taranchuk4@yahoo.com>
2025-03-05 21:22:38 +0100

    feat: add scrollbar styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
+
 import { store } from './redux/store.ts';
 import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
